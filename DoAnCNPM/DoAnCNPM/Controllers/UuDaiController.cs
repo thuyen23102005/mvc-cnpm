57637d4aba91c23549bd6ca34cb3f2f7cf2fd9a6
Pham Minh Triet <trietpham0909@gmail.com>
2025-07-18 12:25:31 +0700

    UuDai

---
+++ DoAnCNPM/DoAnCNPM/Controllers/UuDaiController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnCNPM.Controllers
+{
+    public class UuDaiController : Controller
+    {
+        public ActionResult UuDai()
+        {
+            return View();
+        }
+    }
+}
