c9c28cfbd5d478fc6fc07d131a36e0f1d15eee36
Nguyễn Bình Minh <nbminh8950@gmail.com>
2025-07-18 11:45:29 +0700

    update

---
+++ DoAnCNPM/DoAnCNPM/Controllers/LienHeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnCNPM.Controllers
+{
+    public class LienHeController : Controller
+    {
+        // GET: LienHe
+        public ActionResult LienHe()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LienHe(string hoTen, string email, string noiDung)
+        {
+            // TODO: xử lý dữ liệu, gửi email hoặc lưu database
+            ViewBag.ThongBao = "Cảm ơn bạn đã liên hệ với Circle K! Chúng tôi sẽ phản hồi sớm nhất.";
+            return View();
+        }
+    }
+}
