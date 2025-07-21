a8e202c0abe718a2350b6de6305ffc8febd25f73
Ngo Le Hoang Thuan <ngothuan1805@gmail.com>
2025-07-21 22:04:39 +0700

    GioHang

---
+++ DoAnCNPM/DoAnCNPM/Controllers/GioHangController.cs
@@ -0,0 +1,18 @@
+﻿using DoAnCNPM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DoAnCNPM.Controllers
+{
+    public class GioHangController : Controller 
+    {
+        public ActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
