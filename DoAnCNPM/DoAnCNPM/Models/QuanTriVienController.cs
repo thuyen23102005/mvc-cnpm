a7a45e272296e666c00010b16bf97ee249747d97
nguyen binh minh <nbminh8950@gmail.com>
2025-07-21 11:43:55 +0700

    test

---
+++ DoAnCNPM/DoAnCNPM/Models/QuanTriVienController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnCNPM.Models
+{
+    public class QuanTriVien
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string TenSanPham { get; set; }
+
+        [Required]
+        public string Loai { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal Gia { get; set; }
+
+        public string HinhAnh { get; set; }
+    }
+}
