f0f77dea65ab03d458d0af85ccd86d1a9db7287c
thuyen23102005 <thuyendangminh11a221@gmail.com>
2025-07-25 23:06:32 +0700

    add

--- DoAnCNPM/DoAnCNPM/Controllers/AccountController.cs
+++ DoAnCNPM/DoAnCNPM/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DoAnCNPM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,55 @@ namespace DoAnCNPM.Controllers
     public class AccountController : Controller
     {
         // GET: Account
-        public ActionResult Index()
+        StoreEntities db = new StoreEntities();
+        public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Login model)
+        {
+            var user = db.AdminUsers
+                .FirstOrDefault(u => u.UserName == model.UserName && u.PasswordUser.Trim() == model.PasswordUser);
+
+            if (user != null)
+            {
+                Session["UserName"] = user.UserName;
+                Session["Role"] = user.RoleUser;
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Sai tài khoản hoặc mật khẩu.";
+            return View();
+        }
+
+        // GET: Register
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Register model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new AdminUser
+                {
+                    UserName = model.UserName,
+                    PasswordUser = model.Password,
+                    RoleUser = "1" // Gán mặc định là user
+                };
+
+                db.AdminUsers.Add(user);
+                db.SaveChanges();
+
+                return RedirectToAction("Login");
+            }
+
+            return View(model);
         }
     }
 }
---
+++ DoAnCNPM/DoAnCNPM/Models/Login.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DoAnCNPM.Models
+{
+    public class Login
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Tên đăng nhập là bắt buộc")]
+        public string UserName { get; set; }
+
+        public string RoleUser { get; set; }
+
+        [Required(ErrorMessage = "Mật khẩu là bắt buộc")]
+        [DataType(DataType.Password)]
+        public string PasswordUser { get; set; }
+    }
+}
