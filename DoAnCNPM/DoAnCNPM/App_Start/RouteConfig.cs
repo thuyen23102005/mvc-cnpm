e53fb6eeca22db47fb54e34258cacd8106c09cc0
thuyen23102005 <thuyendangminh11a221@gmail.com>
2025-08-01 22:35:35 +0700

    add

--- DoAnCNPM/DoAnCNPM/App_Start/RouteConfig.cs
+++ DoAnCNPM/DoAnCNPM/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DoAnCNPM
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "OrderProes", action = "Index", id = UrlParameter.Optional }
             );
             routes.MapRoute(
                 name: "ThucUong",
